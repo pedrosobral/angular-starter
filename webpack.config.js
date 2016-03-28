cf0d7968d6f6036f984e224b8896d830141c1800
Pedro Henrique <pedrosobralxv@gmail.com>
2016-03-27 23:30:47 -0300

    add config with html-webpack-plugin. Output => /dist

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var webpack = require("webpack");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    debug: true,
+    minimize: true,
+    entry: {
+        main: './src/app/app.module.js'
+    },
+    output: {
+        path: __dirname + '/dist',
+        filename: 'bundle.js'
+    },
+    plugins: [
+        // Injects bundles in your index.html instead of wiring all manually.
+        // It also adds hash to all injected assets so we don't have problems
+        // with cache purging during deployment.
+        new HtmlWebpackPlugin({
+            template: 'src/index.html',
+            inject: 'body',
+            hash: true
+        }),
+    ]
+};
