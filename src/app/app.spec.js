a732a912edf8674fa6a2ce1175b2a635f2a1a998
Pedro Henrique <pedrosobralxv@gmail.com>
2016-03-28 17:15:12 -0300

    add basic test to show it works"

---
+++ src/app/app.spec.js
@@ -0,0 +1,11 @@
+var app = require('./app.module.js');
+
+describe('App', function() {
+
+    beforeEach(angular.mock.module(app.name));
+
+    it('it should have the correct id', function() {
+    });
+
+
+});
