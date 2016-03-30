c06a197c6f7ee5e9cc91e17806715b5e7d77b65e
Pedro Henrique <pedrosobralxv@gmail.com>
2016-03-30 11:25:49 -0300

    toolbar unit test

--- src/app/app.spec.js
+++ src/app/app.spec.js
@@ -6,9 +6,10 @@ describe('App', () => {
     beforeEach(angular.mock.module(app.name));
 
     beforeEach(inject(($rootScope, $compile) => {
-        scope   = $rootScope.$new();
+        scope = $rootScope.$new();
         element = angular.element('<app></app>');
         element = $compile(element)(scope);
+        scope.$apply();
     }));
 
     it('it should show hello world', () => {
@@ -16,4 +17,9 @@ describe('App', () => {
         expect(h1.text()).toBe('Hello World');
     });
 
+    it('it should set the toolbar', () => {
+        var span = element.find('h2');
+        expect(span).toBeDefined();
+        expect(span.text()).toBe('Angular Starter');
+    });
 });
