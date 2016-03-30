bd38a53aa03b04ec29ce1738cdef94454f33f148
Pedro Henrique <pedrosobralxv@gmail.com>
2016-03-30 10:28:54 -0300

    simple unit test component

--- src/app/app.spec.js
+++ src/app/app.spec.js
@@ -2,10 +2,18 @@ import app from './app.module';
 
 describe('App', () => {
 
+    var scope, element;
     beforeEach(angular.mock.module(app.name));
 
-    it('it should have the correct id', () => {
-    });
+    beforeEach(inject(($rootScope, $compile) => {
+        scope   = $rootScope.$new();
+        element = angular.element('<app></app>');
+        element = $compile(element)(scope);
+    }));
 
+    it('it should show hello world', () => {
+        var h1 = element.find('h1');
+        expect(h1.text()).toBe('Hello World');
+    });
 
 });
